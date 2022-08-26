eb4a7e20dadea3e581537cdb8e43a31b57c0de4f
mis <mearlboro@protonmail.com>
2022-08-26 22:24:00 +0100

    Update homepage slideshow
    - add slideshow for mobiles
    - use images and show them randomly with home.js
    - add styles for home slide in home.css
    - add images in assets/img/home
    - remove assets/video

---
+++ assets/js/home.js
@@ -0,0 +1,35 @@
+var images = [];
+var img = 1;
+var length;
+var elem = document.getElementById('home-slide');
+
+function getImages(mobile) {
+  if (mobile) {
+    for (var i = 1; i <= 33; i++) {
+        images.push('/assets/img/home/S' + i + '.jpg');
+    }
+  }
+  else {
+    for (var i = 1; i <= 43; i++) {
+        images.push('/assets/img/home/L' + i + '.jpg');
+    }
+  }
+}
+
+function changeImage()
+{
+    var old = img
+    while (old == img) {
+        img = Math.floor( Math.random( ) * length );
+    }
+    elem.style.backgroundImage="url(" + images[img] + ")";
+}
+
+window.onload = function() {
+    getImages(window.innerWidth >= 768 ? 0 : 1);
+    length = images.length;
+    var elem = document.getElementById('home-slide');
+    if (elem) {
+        window.setInterval(changeImage, 3000);
+    }
+}
--- index.md
+++ index.md
@@ -4,9 +4,12 @@ home: 1
 
 <article>
 
-<div class="video-full" data-src="/assets/img/APS001.jpg" data-video="/assets/video/AP_Website_Slideshow_1.ogv" data-placeholder="/assets/img/APS001.jpg">
+<div id="home-slide" class="video-full">
+</div>
+
+<!--div class="video-full" data-src="/assets/img/APS001.jpg" data-video="/assets/video/AP_Website_Slideshow_1.ogv" data-placeholder="/assets/img/APS001.jpg">
 <video preload="auto" autoplay="autoplay" muted="muted" loop="" poster="/assets/img/APS001.jpg"><source src="/assets/video/AP_Website_Slideshow_1.ogv" type="video/ogg">
 </video>
-</div>
+</div-->
 
 </article>
